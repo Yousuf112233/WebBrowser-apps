ac1acf5f80485e5930d9a5b5b925e0f2339d153f
This pc <yousuf01865452954@gmail.com>
2023-02-21 18:00:37 +0600

    on click button

--- app/src/main/java/com/webbrowser/MainActivity.java
+++ app/src/main/java/com/webbrowser/MainActivity.java
@@ -3,22 +3,42 @@ package com.webbrowser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    WebView mwebView;
+    Button clickbutton;
+    TextView mytexview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mytexview =findViewById(R.id.mytexview);
+        clickbutton =findViewById(R.id.clickbutton);
+        clickbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        mwebView=findViewById(R.id.mwebView);
-        mwebView.getSettings().setJavaScriptEnabled(true);
-        mwebView.loadUrl("https://www.linkedin.com/company/ifad-multi-products-limited/?originalSubdomain=bd");
+                mytexview.setText("allah Rohom koro onack kosto holo");
+            }
+        });
+
+        mytexview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickbutton.setText("allah khoma koro");
+            }
+        });
+
+
+
+
+
 
 
 
